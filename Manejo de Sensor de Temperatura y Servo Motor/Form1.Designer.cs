bd5d2b0fd28bd620f18de292ec061635b5d0afdb
Yuanhua C.M. <chiuyuanhua@gmail.com>
2024-11-07 19:41:11 -0600

    .

--- Manejo de Sensor de Temperatura y Servo Motor/Form1.Designer.cs
+++ Manejo de Sensor de Temperatura y Servo Motor/Form1.Designer.cs
@@ -36,11 +36,14 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(84, 274);
+            this.button1.Location = new System.Drawing.Point(80, 478);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(182, 101);
             this.button1.TabIndex = 0;
@@ -50,7 +53,7 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(526, 274);
+            this.button2.Location = new System.Drawing.Point(522, 478);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(182, 101);
             this.button2.TabIndex = 1;
@@ -60,15 +63,16 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(84, 128);
+            this.textBox1.Location = new System.Drawing.Point(80, 332);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(182, 26);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(80, 87);
+            this.label1.Location = new System.Drawing.Point(76, 291);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(217, 20);
             this.label1.TabIndex = 3;
@@ -78,35 +82,71 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(80, 204);
+            this.label2.Location = new System.Drawing.Point(76, 408);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(124, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "SIN CONEXIÓN";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(522, 128);
+            this.label3.Location = new System.Drawing.Point(518, 332);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(134, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "TEMPERATURA:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(600, 195);
+            this.label4.Location = new System.Drawing.Point(539, 399);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(31, 20);
+            this.label4.Size = new System.Drawing.Size(51, 20);
             this.label4.TabIndex = 6;
-            this.label4.Text = "0.0";
+            this.label4.Text = "0.0 C°";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(653, 399);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "0.0 F°";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(89, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 89);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "ENCENDER LED";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(500, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(184, 89);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "APAGAR LED";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 626);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -130,6 +170,9 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Manejo de Sensor de Temperatura y Servo Motor/Form1.cs
+++ Manejo de Sensor de Temperatura y Servo Motor/Form1.cs
@@ -15,7 +15,7 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
 {
     public partial class Form1 : Form
     {
-        delegate void SetTextDelegate(string value);
+        delegate void SetTextDelegate(string celsius, string fahrenheit);
         public SerialPort ArduinoPort
         {
             get;
@@ -30,28 +30,51 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
             ArduinoPort.ReadTimeout = 1000;
             ArduinoPort.WriteTimeout = 1000;
             ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+           
             // ArduinoPort.Open();
         }
 
         private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             string dato = ArduinoPort.ReadLine();
-            EscribirText(dato);
+            ProcesarTemperatura(dato);
         }
 
-        private void EscribirText(string dato)
+        private void ProcesarTemperatura(string dato)
+        {
+           
+            if (double.TryParse(dato, out double tempC))
+            {
+               
+                double tempF = (tempC * 1.8) + 32;
+                EscribirText(tempC.ToString("0.0"), tempF.ToString("0.0"));
+            }
+            else
+            {
+              
+                EscribirText("Error", "Error");
+            }
+        }
+
+        private void EscribirText(string celsius, string fahrenheit)
         {
             if (InvokeRequired)
                 try
                 {
-                    Invoke(new SetTextDelegate(EscribirText), dato);
+                    
+                    Invoke(new SetTextDelegate(EscribirText), celsius, fahrenheit);
                 }
                 catch
                 {
-                    //Poner acción de aviso de error
+                  
                 }
             else
-                label4.Text = dato;
+            {
+              
+                label4.Text = celsius + " °C";
+                label5.Text = fahrenheit + " °F";
+            }
         }
 
 
@@ -62,16 +85,13 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //button2.Enabled=true;
-            //button2.Enabled=false;
             try
             {
-
                 if (!ArduinoPort.IsOpen)
                     ArduinoPort.Open();
+
                 if (int.TryParse(textBox1.Text, out int temperatureLimit))
                 {
-
                     string limitString = temperatureLimit.ToString();
                     ArduinoPort.Write(limitString);
                 }
@@ -97,9 +117,45 @@ namespace Manejo_de_Sensor_de_Temperatura_y_Servo_Motor
                 ArduinoPort.Close();
             label2.Text = "Desconectado";
             label2.ForeColor = System.Drawing.Color.Red;
-            label4.Text = "00,0";
+            label4.Text = "00.0 °C";
+            label5.Text = "00.0 °F";
+        }
+     
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
     }
 }
 
